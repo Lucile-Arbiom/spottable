b6cb7126bba85b9879dad00768f4f50f1cb7d708
lucile-D97 <lucile.ducouret@hotmail.fr>
2026-01-08 15:15:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Mon Guide Perso", layout="wide")
 st.title("🍴 Mon Répertoire de Restaurants & Bars")
 
 # Chargement des données
-df = pd.read_csv("adresses.csv")
+df = pd.read_csv("spottable v1.xlsx")
 
 # Extraction des tags uniques pour le filtre
 all_tags = set()
