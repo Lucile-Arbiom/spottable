a61da5126a81ecd1fe64dc13b36db68f66ba3b03
lucile-D97 <lucile.ducouret@hotmail.fr>
2026-01-13 11:19:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown(f"""
     /* 1. Base non sélectionnée (Fond #efede1 + Contour #b6beb1) */
     div[data-testid="stExpander"] {{
         background-color: #efede1 !important;
-        border: 0.5px solid #b6beb1 !important;
+        border: 0.25px solid #b6beb1 !important;
         border-radius: 8px !important;
         margin-bottom: 10px !important;
     }}
